2cc24e3b4112e28c3f3e40df2d9ee755189b5b92
DESKTOP-4JBSCK5\vince <vincentj@naver.com>
2018-07-31 01:55:41 +0900

    chapter 21 Generics(1)

---
+++ chapter21/src/GenericBoc.java
@@ -0,0 +1,48 @@
+class DBox<L ,R>{
+	private L left;
+	private R right;
+	public void set(L o, R r) {
+		left=o;
+		right= r;
+		}
+	public String toString() { return left + " & " + right;}
+}
+
+//문제 2번
+public class GenericBoc {
+
+	public static void main(String[] args) {
+		DBox<String, Integer> box1= new DBox<>();
+		box1.set("apple", 25);
+		DBox<String, Integer> box2=new DBox<>();
+		box2.set("orange", 33);
+		DBox<DBox<String,Integer>, DBox<String,Integer>> dbox = new DBox<>();
+		dbox.set(box1,box2);
+		System.out.println(dbox);
+	}
+}
+//문제 1번
+/**
+class DDBox<U,D>{
+	private U up;
+	private D down;
+	public void set(U o, D r) {
+		up=o;
+		down=r;
+	}
+	public String toString() { return up + "\n" + down;}
+}
+
+public class GenericBoc {
+
+	public static void main(String[] args) {
+		DBox<String, Integer> box1= new DBox<>();
+		box1.set("apple", 25);
+		DBox<String, Integer> box2=new DBox<>();
+		box2.set("orange", 33);
+		DDBox<DBox<String,Integer>, DBox<String,Integer>> ddbox = new DDBox<>();
+		ddbox.set(box1,box2);
+		System.out.println(ddbox);
+	}
+}
+**/
---
+++ chapter21/src/SwapBox.java
@@ -0,0 +1,28 @@
+class Box<T>{
+	private T ob;
+	public void set(T o) { ob = o;}
+	public T get(){ return ob;}
+}
+
+public class SwapBox {
+	
+
+	public static<T extends Number> void swapBox(Box<T> b1, Box<T> b2) {
+		T temp=b1.get();
+		b1.set(b2.get());
+		b2.set(temp);
+		
+	}
+
+	public static void main(String[] args) {
+		Box<Integer> box1= new Box<>();
+		box1.set(99);
+		Box<Integer> box2= new Box<>();
+		box2.set(55);
+		System.out.println(box1.get() + " & "+ box2.get());
+		swapBox(box1, box2);
+		System.out.println(box1.get()+ " & "+box2.get());
+
+	}
+
+}
